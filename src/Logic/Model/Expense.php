5f81c65e8d323341d9f5d4d5e5ccf6eb2b3ff6d0
martincodes-de <martin.schneider@techniknews.net>
2022-05-05 11:38:41 +0200

    rename category attribute

--- src/Logic/Model/Expense.php
+++ src/Logic/Model/Expense.php
@@ -5,10 +5,18 @@ namespace src\Logic\Model;
 final class Expense
 {
     public function __construct(
-        private readonly string $description,
-        private string $tag,
-        private readonly string $price,
+        public readonly string $description,
+        private string $category,
+        public readonly string $price,
     )
     {
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory(): string
+    {
+        return $this->category;
     }
 }
