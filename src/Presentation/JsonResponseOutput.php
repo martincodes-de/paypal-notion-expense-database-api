87dfcfd0984001adc46775befd9168e0e7c381da
martincodes-de <martin.schneider@techniknews.net>
2022-05-05 10:16:19 +0200

    add jsonResponseOutput and NotionExpenseApplicatio

---
+++ src/Presentation/JsonResponseOutput.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace src\Presentation;
+
+class JsonResponseOutput
+{
+    public function __construct(
+        private array $outputJson,
+    )
+    {
+        header('Content-type: application/json');
+        echo json_encode($this->outputJson);
+    }
+}
