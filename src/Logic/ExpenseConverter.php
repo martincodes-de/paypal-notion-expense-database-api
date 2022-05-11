6e7267cdb57e4bc3681483ec072788fca4c12b1f
martincodes-de <martin.schneider@techniknews.net>
2022-05-11 21:05:41 +0200

    added converter methods

--- src/Logic/ExpenseConverter.php
+++ src/Logic/ExpenseConverter.php
@@ -2,7 +2,43 @@
 
 namespace src\Logic;
 
+use src\Logic\Model\Expense;
+use stdClass;
+
 final class ExpenseConverter
 {
+    /**
+     * @param stdClass[] $rawExpenses
+     * @return Expense[]
+     */
+    public function convertToExpenses(array $rawExpenses): array
+    {
+        $convertedExpenses = [];
 
+        foreach ($rawExpenses as $rawExpense) {
+            $rawExpense = (array)$rawExpense;
+            if ($this->isConvertable($rawExpense)) {
+                $convertedExpenses[] = $this->convertSingleToExpense($rawExpense);
+            }
+        }
+
+        return $convertedExpenses;
+    }
+
+    private function convertSingleToExpense(array $rawExpense): Expense
+    {
+        return new Expense($rawExpense["description"], "undefined", $rawExpense["price"]);
+    }
+
+    private function isConvertable(array $rawExpense): bool
+    {
+        if (array_key_exists("description", $rawExpense)
+            && array_key_exists("price", $rawExpense)
+            && array_key_exists("notice", $rawExpense)
+        ) {
+            return true;
+        }
+
+        return false;
+    }
 }
